f32b9f185847e788f6c0a2afc03ab8738a515324
deepali <sawantalka@gmail.com>
2020-03-04 19:20:47 +0530

    c

--- src/app/dashboard-loan/dashboard-loan.component.ts
+++ src/app/dashboard-loan/dashboard-loan.component.ts
@@ -10,20 +10,62 @@ import { Label } from 'ng2-charts';
 export class DashboardLoanComponent implements OnInit {
   public barChartOptions: ChartOptions = {
     responsive: true,
+    scales: {
+      xAxes: [{
+        gridLines: {
+
+          display: false
+        }
+      }],
+      yAxes: [{
+        gridLines: {
+
+          display: false
+        }
+      }]
+    },
+    legend: {
+      display: false,
+    },
+    
   };
+  
   public barChartLabels: Label[] = ['Project1', 'Project2', 'Project3', 'Project4', 'Project5', 'Project6', 'Project7'];
   public barChartType: ChartType = 'bar';
+
   public barChartLegend = true;
   public barChartPlugins = [];
 
   public barChartData: ChartDataSets[] = [
+
     // { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' }
-    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B', }
-    
+    {
+      data: [250, 400, 500, 600, 700, 800, 680],
+
+      backgroundColor: [
+        'blue',
+        'blue',
+        'blue',
+        'blue',
+        'blue',
+        'blue',
+        'blue'
+      ],
+      barThickness: 20,
+
+
+    },
+
+
+
+
+
   ];
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
