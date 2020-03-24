2fe0617fdfc7a0c5c56e65969ed82a6f57e79995
mudassir <mudassir7pwalab@gmail.com>
2020-03-24 13:31:06 +0530

    header change

--- src/app/all-projects/all-projects.component.ts
+++ src/app/all-projects/all-projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../service.service';
 
 @Component({
   selector: 'app-all-projects',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllProjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shared : ServiceService) { }
 
   ngOnInit(): void {
+    this.shared.headerTitle('List All Projects');
   }
 
 }
--- src/app/raise-demand/raise-demand.component.ts
+++ src/app/raise-demand/raise-demand.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../service.service';
 
 @Component({
   selector: 'app-raise-demand',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RaiseDemandComponent implements OnInit {
 
-  constructor() { }
+  constructor(private shared : ServiceService) { }
 
   ngOnInit(): void {
+    this.shared.headerTitle('Raise Demand');
   }
 
 }
