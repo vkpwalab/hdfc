f6acfe9ffc6ff59c85e3cb84d93f658c89d02430
dipesh <dipesh@snsepro.com>
2020-05-14 12:58:51 +0530

    reach us ts

--- src/app/reach-us/reach-us.component.ts
+++ src/app/reach-us/reach-us.component.ts
@@ -27,7 +27,7 @@ export class ReachUsComponent implements OnInit {
 
 
 
-  constructor(private shared: SharedService, private fb: FormBuilder) {}
+  constructor(private shared: SharedService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
 
@@ -49,9 +49,9 @@ export class ReachUsComponent implements OnInit {
       'enable_loan_assistance': [''],
       'enable_project_site': ['']
 
-      
+
     })
-    
+
   }
 
   getBuilersDetails() {
@@ -85,29 +85,29 @@ export class ReachUsComponent implements OnInit {
   getPacProjectList() {
     let body_Pac_Project_List = { branch: this.branch_no, I_BUILDER_ID: '510673', Token: 'MH3NPYK34J0KHDI' };
 
-  
-      (<any>this.shared.client).Get__Pac_Project_List(body_Pac_Project_List).subscribe(
-        (res: ISoapMethodResponse) => {
-          console.log('method response', res);
-          let xmlResponse = res.xml;
-          let result = res.result.Get__Pac_Project_ListResult;
-          console.log(result);
 
-          var result_json = JSON.parse(result)
-          console.log("object", result_json)
+    (<any>this.shared.client).Get__Pac_Project_List(body_Pac_Project_List).subscribe(
+      (res: ISoapMethodResponse) => {
+        console.log('method response', res);
+        let xmlResponse = res.xml;
+        let result = res.result.Get__Pac_Project_ListResult;
+        console.log(result);
 
-          this.project_list = result_json.Table;
-        },
-        err => console.log(err)
-      );
-    
+        var result_json = JSON.parse(result)
+        console.log("object", result_json)
+
+        this.project_list = result_json.Table;
+      },
+      err => console.log(err)
+    );
+
 
   }
 
-  
-  
 
-  
+
+
+
 
 
 
@@ -115,29 +115,44 @@ export class ReachUsComponent implements OnInit {
   selectProjectOption() {
     console.log(this.select_values_of_project)
   }
-  
+
 
   submitReachUsForm(data) {
-   
-    let body_reach_us_form = { I_proj_appr_needed: data.remark_approval, I_camp_managment: data.remark_campaign, I_suventn_scheme: data.remark_subvention_scheme, I_spec_disb_facility: data.remark_disbursment_facility, I_onsile_hmloan_assis: data.remark_home_loan, I_full_time_sales: data.remark_sales_executive, Token: 'MH3NPYK34J0KHDI', Project_Name: data.select_values_of_project, status_approval:data.enable_project_approval , status_management: data.enable_campaign_management, status_subvention: data.enable_subvention_scheme, status_disbursment: data.enable_disbursment_facility, status_loan: data.enable_loan_assistance, status_project: data.enable_project_site }
+
+    let body_reach_us_form = {
+      I_proj_appr_needed: data.remark_approval,
+      I_camp_managment: data.remark_campaign,
+      I_suventn_scheme: data.remark_subvention_scheme,
+      I_spec_disb_facility: data.remark_disbursment_facility,
+      I_onsile_hmloan_assis: data.remark_home_loan,
+      I_full_time_sales: data.remark_sales_executive,
+      Token: 'MH3NPYK34J0KHDI',
+      Project_Name: data.select_values_of_project,
+      status_approval: data.enable_project_approval,
+      status_management: data.enable_campaign_management,
+      status_subvention: data.enable_subvention_scheme,
+      status_disbursment: data.enable_disbursment_facility,
+      status_loan: data.enable_loan_assistance,
+      status_project: data.enable_project_site
+    }
     console.log(body_reach_us_form)
     console.log(data);
     setTimeout(() => {
       if (this.reachus.valid) {
 
-      (<any>this.shared.client).Insert_proj_assistance(body_reach_us_form).subscribe(
-        (res: ISoapMethodResponse) => {
-          console.log('method response', res);
-          let xmlResponse = res.xml;
-          alert('Thank you.Your Reach-Us Form is Submitted Successfully.')
-          location.reload();
+        (<any>this.shared.client).Insert_proj_assistance(body_reach_us_form).subscribe(
+          (res: ISoapMethodResponse) => {
+            console.log('method response', res);
+            let xmlResponse = res.xml;
+            alert('Thank you.Your Reach-Us Form is Submitted Successfully.')
+            location.reload();
 
 
-        },
-        (err) => console.log(err)
-      );
+          },
+          (err) => console.log(err)
+        );
       }
-      else{
+      else {
         alert('Please Select Project')
       }
     }, 4000);
