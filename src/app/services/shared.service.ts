192978d108c063f1e0a0579148da64e8cef05bd2
USER <gaurav.pwalabs@gmail.com>
2020-05-22 11:56:24 +0530

    c

--- src/app/services/shared.service.ts
+++ src/app/services/shared.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 import { NgxSoapService, Client, ISoapMethodResponse } from 'ngx-soap';
+import { ActivatedRoute } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedService {
   client: Client;
-  constructor(private soap: NgxSoapService) { 
+  path: string;
+  
+  constructor(private soap: NgxSoapService,private ar:ActivatedRoute) { 
     this.soap.createClient('assets/hdfc-api.wsdl')
     .then(client => {
       // client.addHttpHeader('Access-Control-Allow-Origin', '*');
@@ -24,7 +27,19 @@ export class SharedService {
   headerTitle(v) {
     this.header_title.next(v)
   }
-
+  peoplePage=new BehaviorSubject('People')
+  peoplePageProAss(v)
+  {
+    this.peoplePage.next(v)
+  }
+  // mypath=new BehaviorSubject('')
+  // myOverview(data){
+  //   this.mypath.next(data) 
+  // }
+  // mypath1=new BehaviorSubject('')
+  // myOverview1(data){
+  //   this.mypath1.next(data) 
+  // }
   isMobile() {
     {
       return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
