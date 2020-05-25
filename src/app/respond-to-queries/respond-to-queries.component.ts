4e2f0fa801cd693c73468e99d0b850996cf84867
dipesh <dipesh@snsepro.com>
2020-05-25 11:19:53 +0530

    response

--- src/app/respond-to-queries/respond-to-queries.component.ts
+++ src/app/respond-to-queries/respond-to-queries.component.ts
@@ -8,59 +8,45 @@ import { ISoapMethodResponse } from 'ngx-soap';
   styleUrls: ['./respond-to-queries.component.css']
 })
 export class RespondToQueriesComponent implements OnInit {
-  builders_details: any = [];
   branch_no: any;
   project_list: any;
   query_list: any;
-  array_query_type: any = [];
-  query_type: any = [];
-  builder_query_type: any = [];
-  builer_id: any = [];
-  new_builders_details: any = [];
-  newarray: any = [];
-
+  project_selected: any;
+  builder_detail: any;
+  builder_names:any = {};
+  query_data: any = [];
+  raised_by: any = "";
+  message:any = '';
+  
   constructor(private shared: SharedService) { }
 
   ngOnInit(): void {
     this.shared.headerTitle('Respond To Queries');
-    // this.getBuilersDetails()
-    this.getQueryList()
-    // this.getBuilersDetails1()
+    this.getBuilersDetails();
   }
 
-  getBuilersDetails(v) {
-    let body_builders_details = { BUILDERID: v, Token: 'MH3NPYK34J0KHDI' };
+  getBuilersDetails() {
+    let body_builders_details = { BUILDERID: '510673', Token: 'MH3NPYK34J0KHDI' };
     console.log(body_builders_details)
 
     setTimeout(() => {
-
       (<any>this.shared.client).GetBuilderDetails(body_builders_details).subscribe(
 
         (res: ISoapMethodResponse) => {
-          console.log('method response', res);
           let xmlResponse = res.xml;
           let result = res.result.GetBuilderDetailsResult;
-          // console.log(result);
 
           var result_json = JSON.parse(result)
-          console.log("object", result_json)
 
+          this.builder_detail = result_json.Table;
+          console.log(this.builder_detail);
 
-          this.builders_details = result_json.Table
-          console.log(this.builders_details)
-          this.new_builders_details = this.builders_details.map(e => e.BUILDER_NAME)
-          console.log(this.new_builders_details)
-          for (var i = 0; i < this.new_builders_details.length; i++) {
-            this.newarray = this.new_builders_details[i]
-            console.log(this.newarray)
-          }
-          // this.branch_no = this.builders_details[0].BRANCH_NO;
+          this.branch_no = this.builder_detail[0].BRANCH_NO;
 
           this.getPacProjectList();
         },
         err => console.log(err)
       );
-
     }, 1000);
   }
 
@@ -69,16 +55,11 @@ export class RespondToQueriesComponent implements OnInit {
   getPacProjectList() {
     let body_Pac_Project_List = { branch: this.branch_no, I_BUILDER_ID: '510673', Token: 'MH3NPYK34J0KHDI' };
 
-
     (<any>this.shared.client).Get__Pac_Project_List(body_Pac_Project_List).subscribe(
       (res: ISoapMethodResponse) => {
-        console.log('method response', res);
         let xmlResponse = res.xml;
         let result = res.result.Get__Pac_Project_ListResult;
-        console.log(result);
-
         var result_json = JSON.parse(result)
-        console.log("object", result_json)
 
         this.project_list = result_json.Table;
       },
@@ -88,65 +69,77 @@ export class RespondToQueriesComponent implements OnInit {
 
   }
 
-  getQueryList() {
+  projectChange(event) {
+    this.project_selected = event.value;
+
     let body_P_Get_Query_Data = { I_PROJECT_NO: '575029', Token: 'MH3NPYK34J0KHDI' };
 
-    setTimeout(() => {
+    (<any>this.shared.client).P_Get_Query_Data(body_P_Get_Query_Data).subscribe(
+      (res: ISoapMethodResponse) => {
+        let xmlResponse = res.xml;
+        let result = res.result.P_Get_Query_DataResult;
+        var result_json = JSON.parse(result)
 
-      (<any>this.shared.client).P_Get_Query_Data(body_P_Get_Query_Data).subscribe(
+        this.query_list = result_json.Table;
+
+        
+        let count = 0;
+        this.query_list.forEach(element => {
+            this.getBuilderName(element.CREATED_BY,element.QUERY_TYPE);
+        });
+        
+        console.log(this.query_list)
+        // console.log(this.builders_details);
+      },
+      err => console.log(err)
+    );
+
+  }
+
+  getBuilderName(id,query_type){
+
+    let body_builders_details = { BUILDERID: id, Token: 'MH3NPYK34J0KHDI' };
+
+    if(query_type == 'Builder'){
+      (<any>this.shared.client).GetBuilderDetails(body_builders_details).subscribe(
+
         (res: ISoapMethodResponse) => {
-          console.log('method response', res);
           let xmlResponse = res.xml;
-          let result = res.result.P_Get_Query_DataResult;
-          console.log(result);
+          let result = res.result.GetBuilderDetailsResult;
 
           var result_json = JSON.parse(result)
-          console.log("object", result_json)
 
-          this.query_list = result_json.Table;
-
-          console.log(this.query_list)
-          for (var i = 0; i < this.query_list.length; i++) {
-           
-
-            console.log("query type", this.query_list[i].QUERY_TYPE);
-
-            if (this.query_list[i].QUERY_TYPE == 'Builder') {
-
-              console.log(this.query_list[i].CREATED_BY)
-
-             this.getBuilersDetails(this.query_list[i].CREATED_BY)
-              this.query_list = this.query_list.map((bldg) => {
-                for (var j = 0; j < this.builders_details.length; j++) {
-                  let eObject = Object.assign({}, bldg);
-
-                  eObject.BUILDER_NAME = this.new_builders_details[j];
-                  return eObject;
-
-                }
-              })
+          let builder_detail = result_json.Table;
  
-            } else {
-
-
-              this.query_list = this.query_list.map((bldg) => {
-                for (var j = 0; j < this.builders_details.length; j++) {
-                  let eObject = Object.assign({}, bldg);
-
-                  eObject.BUILDER_NAME = this.query_list[i].CREATED_BY;
-                  return eObject;
-
-                }
-              })
-            }
-          }
-          // console.log(this.builders_details);
+          this.builder_names[id] = builder_detail[0].BUILDER_NAME;
+          
         },
         err => console.log(err)
       );
-
-    }, 1000);
-
+    }else{
+      this.builder_names[id] = id;
+    }
   }
+
+  queryData(id,raised_by){
+    this.raised_by = raised_by;
+    let body_query_detail = { I_QUEST_ID: id, Token: 'MH3NPYK34J0KHDI' };
+
+    (<any>this.shared.client).Get_QRY_DTA_BY_QUESTID(body_query_detail).subscribe(
+
+      (res: ISoapMethodResponse) => {
+        let xmlResponse = res.xml;
+        let result = res.result.Get_QRY_DTA_BY_QUESTIDResult;
+
+        var result_json = JSON.parse(result)
+
+        this.query_data = result_json.Table;
+        console.log(this.query_data);
+        
+      },
+      err => console.log(err)
+    );
+  }
+
 
 }
--- src/app/update-work/update-work.component.ts
+++ src/app/update-work/update-work.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import $ from 'jquery';
 import { SharedService } from '../services/shared.service';
 import { ISoapMethodResponse } from 'ngx-soap';
-import { ActivatedRoute } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+
 
 @Component({
   selector: 'app-update-work',
@@ -14,12 +15,11 @@ export class UpdateWorkComponent implements OnInit {
   branch_no: any;
   project_list: any = [];
   building_list: any = [];
-  path: string;
-  event_value: any;
-  p_name: any;
-  project_name: any = [];
-  select_values_of_project: any;
-  constructor(private shared: SharedService,private ar:ActivatedRoute) {
+  project_selected: any;
+  upload_doc_type: any;
+  dynamic_forms: any = {};
+  project_selected_detail: any;
+  constructor(private shared: SharedService,private fb: FormBuilder) {
     // this.show_progress=false;
    }
 
@@ -28,14 +28,6 @@ export class UpdateWorkComponent implements OnInit {
 
     this.getBuilersDetails();
   }
-   showProgress(id){
-     if($('#progress'+id).is(':visible')){
-       $('#progress'+id).hide();
-     }else{
-      $('#progress'+id).show();
-     }
-   }
-  
 
   getBuilersDetails(){
     let body_builders_details = { BUILDERID: '510673', Token: 'MH3NPYK34J0KHDI' };
@@ -56,6 +48,7 @@ export class UpdateWorkComponent implements OnInit {
           this.branch_no = this.builder_detail[0].BRANCH_NO;
 
           this.getPacProjectList();
+          this.getUploadDocType();
         },
         err => console.log(err)
       );
@@ -83,13 +76,31 @@ export class UpdateWorkComponent implements OnInit {
 
   }
 
+  getUploadDocType(){
+    let body_upload_doc_type = { I_CD_VAL: 'DEMAND_LTR_DOC_LIST', Token: 'MH3NPYK34J0KHDI' };
+
+    (<any>this.shared.client).Get_MASTER_DATA(body_upload_doc_type).subscribe(
+      (res: ISoapMethodResponse) => {
+        console.log('method response', res);
+        let xmlResponse = res.xml;
+        let result = res.result.Get_MASTER_DATAResult;
+
+        var result_json = JSON.parse(result)
+
+        this.upload_doc_type = result_json.Table;
+
+        console.log(this.upload_doc_type);
+      },
+      err => console.log(err)
+    );
+  }
+
   
   projectChange(event){
-   
-    this.event_value = event.value
-    console.log(this.event_value);
-
-    let body_Building_List = { i_project_no: this.event_value, Token: 'MH3NPYK34J0KHDI' };
+    this.project_selected = event.value;
+    this.project_selected_detail = this.project_list.filter(p=> p.PROJECTID == this.project_selected );
+    
+    let body_Building_List = { i_project_no: this.project_selected, Token: 'MH3NPYK34J0KHDI' };
 
     (<any>this.shared.client).get_project_building(body_Building_List).subscribe(
       (res: ISoapMethodResponse) => {
@@ -99,25 +110,54 @@ export class UpdateWorkComponent implements OnInit {
 
         var result_json = JSON.parse(result)
 
+        
+        result_json.Table.forEach(element => {
+          this.dynamic_forms[element.PROJ_BLDG_NO] = this.fb.group({
+            'update_current_progress': [element.STAGE_OF_CONS, Validators.required],
+            'percent_payment_due': [element.PERC_DUE, Validators.required],
+            'progress_date': [element.PROGRESS, Validators.required],
+            'doc_type': [element.DOCUMENT_TYPE, Validators.required],
+            'build_no':[element.PROJ_BLDG_NO],
+            'build_name':[element.BLDG_NAME],
+          });
+
+        });
+
         this.building_list = result_json.Table;
-
         console.log(this.building_list);
-      //  this.getPacProjectList()
-
-      let project_name = this.project_list.filter(p=> p.PROJECTID == this.event_value )
-
-      console.log(project_name);
-
-      this.building_list = this.building_list.map((bldg)=>{
-        let eObject = Object.assign({}, bldg);
-        eObject.project_name = project_name[0].PROJECT_NAME;
-        return eObject;
-      })
-
-      console.log(this.building_list);
 
       },
       err => console.log(err)
     );
+  }
+
+  updateBuildingData(data){
+    console.log(data);
+    if(this.dynamic_forms[data.build_no].valid){
+      let body_insert_query = { 
+        I_PROJECT_NO: this.project_selected,
+        I_BUILDING_NAME:data.build_name,
+        I_BUILDING_NO: data.build_no,
+        I_PROG_DATE: data.progress_date,
+        I_PROG_REMARKS: data.update_current_progress,
+        I_AMOUNT_DUE: data.percent_payment_due,
+        I_UPLOAD_DOC: data.doc_type,
+        I_IS_DEMAND_LETTER: '',
+        I_CREATED_BY: '510673', 
+        Token: 'MH3NPYK34J0KHDI' };
+        
+        console.log(body_insert_query);
+      (<any>this.shared.client).Save_Bldng_Progress(body_insert_query).subscribe(
+        (res: ISoapMethodResponse) => {
+          console.log('method response', res);
+          let xmlResponse = res.xml;
+          let result = res.result.Save_Bldng_ProgressResult;
+  
+          var result_json = JSON.parse(result)
+          console.log(result_json)
+        },
+        err => console.log(err)
+      );
+    }
   }
 }
