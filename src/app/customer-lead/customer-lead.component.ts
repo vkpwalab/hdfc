112287782434f11671816fd4d1d350f5cb99417b
dipesh <dipesh@snsepro.com>
2020-05-28 16:26:42 +0530

    custmore

--- src/app/customer-lead/customer-lead.component.ts
+++ src/app/customer-lead/customer-lead.component.ts
@@ -14,40 +14,29 @@ import { ISoapMethodResponse } from 'ngx-soap';
 
 
 export class CustomerLeadComponent implements OnInit {
-  
-  select_project = new FormControl('', Validators.required);
+
   fileEntry: boolean = false
   customer_lead_form: FormGroup;
   isSubmitted = false;
-  uploadthroughexcel: FormGroup;
-  filename: string;
   all_state: any;
   list_city: any;
-  state_name: any[];
-  city: any;
   list_interested_project: any;
-  createdby: any;
-  updatedby: any;
-  // projectid: any;
-  tokenid: any;
   project_name: any;
-  ans: number;
-  ans2;
-  select_values_of_state: any;
-  order: any;
-  state_name1: any;
+
+  selected_state: any;
   submit_button: boolean = false;
   builder_details: any;
   branch_no: any;
   project_list: any;
-  select_values_of_project: any;
-  
+  project_selected: any;
+  AllowedFile: any = ['jpeg', 'jpg', 'png'];
+  file: any;
+  file_name: any;
 
   constructor(private shared: SharedService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.getBuilersDetails();
-    this.selectStateValue();
+
     this.shared.headerTitle('Submit Customer Lead');
     this.customer_lead_form = this.fb.group({
       'I_Cust_Name': ['', Validators.required],
@@ -63,243 +52,201 @@ export class CustomerLeadComponent implements OnInit {
       // 'myprofile': ['', Validators.required]
     })
 
-    let body_Get_all_state = { Token: 'MH3NPYK34J0KHDI' }
-    let body_Project_By_Bldrid = { I_BUILDER_ID: '510673', Token: 'MH3NPYK34J0KHDI' }
-
-
-    // for get all state //
     setTimeout(() => {
-
-      (<any>this.shared.client).Get_all_state(body_Get_all_state).subscribe(
-        (res: ISoapMethodResponse) => {
-          console.log('method response', res);
-          let xmlResponse = res.xml;
-          let result = res.result.Get_all_stateResult;
-          // console.log(result);
-
-          var result_json = JSON.parse(result)
-          // console.log("object", result_json)
-
-          this.all_state = result_json.Table;
-          console.log(this.all_state);
-
-
-        },
-        err => console.log(err)
-      );
-    }, 1000);
-    // for get all state //
-
-
-    // for get project name //
-
-    setTimeout(() => {
-      (<any>this.shared.client).Get_Project_By_Bldrid(body_Project_By_Bldrid).subscribe(
-        (res: ISoapMethodResponse) => {
-          console.log('method response', res);
-          let xmlResponse = res.xml;
-          let result = res.result.Get_Project_By_BldridResult;
-          // console.log(message2);
-
-          var result_json = JSON.parse(result)
-          console.log("object", result_json)
-
-          this.list_interested_project = result_json.Table;
-
-
-        },
-        err => console.log(err)
-      );
-    }, 4000);
-
-    // for get project name //
-
-
+      this.getBuilersDetails();
+      this.getAllState();
+      this.getInterestedProj();
+    }, 2000);
   }
 
-  getBuilersDetails(){
+  getBuilersDetails() {
     let body_builders_details = { BUILDERID: '510673', Token: 'MH3NPYK34J0KHDI' };
 
-    setTimeout(() => {
-      (<any>this.shared.client).GetBuilderDetails(body_builders_details).subscribe(
+    (<any>this.shared.client).GetBuilderDetails(body_builders_details).subscribe(
 
-        (res: ISoapMethodResponse) => {
-          console.log('method response', res);
-          let xmlResponse = res.xml;
-          let result = res.result.GetBuilderDetailsResult;
-          console.log(result);
+      (res: ISoapMethodResponse) => {
+        console.log('method response', res);
+        let xmlResponse = res.xml;
+        let result = res.result.GetBuilderDetailsResult;
+        console.log(result);
 
-          var result_json = JSON.parse(result)
-          console.log("object", result_json)
+        var result_json = JSON.parse(result)
+        console.log("object", result_json)
 
 
-          this.builder_details = result_json.Table;
+        this.builder_details = result_json.Table;
 
-          this.branch_no = this.builder_details[0].BRANCH_NO;
+        this.branch_no = this.builder_details[0].BRANCH_NO;
 
-          this.getPacProjectList();
-        },
-        err => console.log(err)
-      );
-    }, 1000);
+        this.getPacProjectList();
+      },
+      err => console.log(err)
+    );
   }
-  getPacProjectList(){
+  getPacProjectList() {
     let body_Pac_Project_List = { branch: this.branch_no, I_BUILDER_ID: '510673', Token: 'MH3NPYK34J0KHDI' };
 
-   
-      (<any>this.shared.client).Get__Pac_Project_List(body_Pac_Project_List).subscribe(
-        (res: ISoapMethodResponse) => {
-          console.log('method response', res);
-          let xmlResponse = res.xml;
-          let result = res.result.Get__Pac_Project_ListResult;
-          console.log(result);
+    (<any>this.shared.client).Get__Pac_Project_List(body_Pac_Project_List).subscribe(
+      (res: ISoapMethodResponse) => {
+        console.log('method response', res);
+        let xmlResponse = res.xml;
+        let result = res.result.Get__Pac_Project_ListResult;
+        console.log(result);
 
-          var result_json = JSON.parse(result)
-          console.log("object", result_json)
+        var result_json = JSON.parse(result)
+        console.log("object", result_json)
 
-          this.project_list = result_json.Table;
+        this.project_list = result_json.Table;
 
-        
-        },
-        err => console.log(err)
-      );
-   
+
+      },
+      err => console.log(err)
+    );
+
 
   }
-  selectProjectOption() {
-    console.log(this.select_values_of_project)
+  selectProjectOption(event) {
+    this.project_selected = event.value;
+    console.log(this.project_selected)
   }
-
-  // Post Data //
 
   submitForm(data) {
 
     let body_customer_lead_data = { I_Cust_Name: data.I_Cust_Name, I_Cust_Email: data.I_Cust_Email, I_Cust_Mobile: data.I_Cust_Mobile, I_CITY: data.I_CITY, I_IP: 'data.I_IP', I_BOOKING_STATUS: data.I_BOOKING_STATUS, I_REMARKS: data.I_REMARKS, I_OTHER_PROJ_INFO: data.I_OTHER_PROJ_INFO, I_STATE: data.I_STATE, Token: 'MH3NPYK34J0KHDI' }
-    console.log(body_customer_lead_data)
-    console.log(data);
-    setTimeout(() => {
-      if (this.customer_lead_form.valid) {
-        this.submit_button = true;
-        (<any>this.shared.client).Create_Customer_Lead(body_customer_lead_data).subscribe(
-          (res: ISoapMethodResponse) => {
-            console.log('method response', res);
-            let xmlResponse = res.xml;
-            alert('Thank you.Your Form is Submitted Successfully.')
-            location.reload();
-
-            // let message = res.result.Create_Customer_LeadResult;
-          },
-          err => console.log(err)
-        );
-      }
-      else{
-        alert('Please fill all the fields.')
-      }
-    }, 4000);
-
-  }
-
-  // Post Data //
-
-
-  selectStateValue() {
-
-    console.log(this.select_values_of_state)
-
-
-    let body_GET_CITY = { City: this.select_values_of_state, Token: 'MH3NPYK34J0KHDI' }
-    console.log(body_GET_CITY)
-    setTimeout(() => {
-      (<any>this.shared.client).GET_CITY(body_GET_CITY).subscribe(
-
+    if (this.customer_lead_form.valid) {
+      this.submit_button = true;
+      (<any>this.shared.client).Create_Customer_Lead(body_customer_lead_data).subscribe(
         (res: ISoapMethodResponse) => {
           console.log('method response', res);
           let xmlResponse = res.xml;
-          let result = res.result.GET_CITYResult;
-          console.log(result);
-
-          var result_json = JSON.parse(result)
-          console.log("object", result_json)
-
-          this.list_city = result_json.Table;
-          console.log(this.list_city)
-
-
+          alert('Thank you.Your Form is Submitted Successfully.')
+          location.reload();
         },
         err => console.log(err)
       );
-    }, 4000);
-  }
-
-
-  public files: NgxFileDropEntry[] = [];
-
-
-  public dropped(files: NgxFileDropEntry[]) {
-    this.files = files;
-    for (const droppedFile of files) {
-
-
-      if (droppedFile.fileEntry.isFile) {
-        const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
-        this.fileEntry = true;
-        fileEntry.file((file: File) => {
-          this.filename = file.name
-          console.log(this.filename);
-
-          console.log(droppedFile.relativePath, file);
-
-
-        });
-      }
-      else {
-
-        const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
-        console.log(droppedFile.relativePath, fileEntry);
-        this.fileEntry = false;
-
-
-      }
+    }
+    else {
+      alert('Please fill all the fields.')
     }
   }
 
-  // get state() {
-  //   return this.customerlead.get('state');
+  selectStateValue(event) {
+    this.selected_state = event.value;
+    let body_GET_CITY = { City: this.selected_state, Token: 'MH3NPYK34J0KHDI' };
+
+    (<any>this.shared.client).GET_CITY(body_GET_CITY).subscribe(
+
+      (res: ISoapMethodResponse) => {
+        console.log('method response', res);
+        let xmlResponse = res.xml;
+        let result = res.result.GET_CITYResult;
+
+        var result_json = JSON.parse(result)
+
+        this.list_city = result_json.Table;
+
+
+      },
+      err => console.log(err)
+    );
+  }
+
+  getAllState() {
+    let body_Get_all_state = { Token: 'MH3NPYK34J0KHDI' };
+    (<any>this.shared.client).Get_all_state(body_Get_all_state).subscribe(
+      (res: ISoapMethodResponse) => {
+        console.log('method response', res);
+        let xmlResponse = res.xml;
+        let result = res.result.Get_all_stateResult;
+
+        var result_json = JSON.parse(result);
+
+        this.all_state = result_json.Table;
+        console.log(this.all_state);
+
+
+      },
+      err => console.log(err)
+    );
+  }
+
+  getInterestedProj() {
+    let body_Project_By_Bldrid = { I_BUILDER_ID: '510673', Token: 'MH3NPYK34J0KHDI' };
+
+    (<any>this.shared.client).Get_Project_By_Bldrid(body_Project_By_Bldrid).subscribe(
+      (res: ISoapMethodResponse) => {
+        console.log('method response', res);
+        let xmlResponse = res.xml;
+        let result = res.result.Get_Project_By_BldridResult;
+        // console.log(message2);
+
+        var result_json = JSON.parse(result)
+        console.log("object", result_json)
+
+        this.list_interested_project = result_json.Table;
+
+
+      },
+      err => console.log(err)
+    );
+
+  }
+
+  ExcelFileEvent($event) {
+    if ($event.target.files[0]) {
+      var file: File = $event.target.files[0];
+      if (!this.validateFile(file)) {
+        alert("Unsupported image format");
+        return false;
+      }
+
+      if (file.size > 4294967296) {
+        alert("Max. File size: 4GB");
+        return false;
+      }
+      this.file = $event.target.files[0];
+      console.log(this.file)
+      this.file_name = this.file.name
+
+
+
+    }
+  }
+  validateFile(file: File) {
+    let extension = file.name.split('.').pop().toLowerCase();
+
+    return this.AllowedFile.indexOf(extension) > -1;
+  }
+  // public files: NgxFileDropEntry[] = [];
+
+
+  // dropped(files: NgxFileDropEntry[]) {
+  //   this.files = files;
+  //   for (const droppedFile of files) {
+
+
+  //     if (droppedFile.fileEntry.isFile) {
+  //       const fileEntry = droppedFile.fileEntry as FileSystemFileEntry;
+  //       this.fileEntry = true;
+  //       fileEntry.file((file: File) => {
+  //         this.filename = file.name
+  //         console.log(this.filename);
+
+  //         console.log(droppedFile.relativePath, file);
+
+
+  //       });
+  //     }
+  //     else {
+
+  //       const fileEntry = droppedFile.fileEntry as FileSystemDirectoryEntry;
+  //       console.log(droppedFile.relativePath, fileEntry);
+  //       this.fileEntry = false;
+
+
+  //     }
+  //   }
   // }
-
-
-
-  soapCall() {
-    const xmlhttp = new XMLHttpRequest();
-
-    // The following variable contains the xml SOAP request.
-    const sr =
-      `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">
-        <soapenv:Header/>
-        <soapenv:Body>
-           <tem:Get_all_state>
-              <!--Optional:-->
-              <tem:Token>MH3NPYK34J0KHDI</tem:Token>
-           </tem:Get_all_state>
-        </soapenv:Body>
-     </soapenv:Envelope>`;
-
-    xmlhttp.onreadystatechange = () => {
-      if (xmlhttp.readyState == 4) {
-        if (xmlhttp.status == 200) {
-          const xml = xmlhttp.responseXML;
-          console.log(xml);
-          // Here I'm getting the value contained by the <return> node.
-          // const response_number = parseInt(xml.getElementsByTagName('return')[0].childNodes[0].nodeValue);
-          // Print result square number.
-          // console.log(response_number);
-        }
-      }
-    }
-    // Send the POST request.
-    // xmlhttp.setRequestHeader('Content-Type', 'text/xml');
-    // xmlhttp.responseType = 'document';
-    xmlhttp.open('GET', 'https://pws.hdfc.com/Devport_dmz/Devport_Out_API.svc?singleWsdl', true);
-    xmlhttp.send(sr);
-  }
 }
+  // http://pws.hdfc.com/Devport_dmz/Devport_Out_API.svc
+  // https://pws.hdfc.com/Devport_dmz/Service1.svc
--- src/app/dashboard/dashboard-projects/dashboard-projects.component.ts
+++ src/app/dashboard/dashboard-projects/dashboard-projects.component.ts
@@ -8,7 +8,7 @@ import { ISoapMethodResponse } from 'ngx-soap';
   styleUrls: ['./dashboard-projects.component.css']
 })
 export class DashboardProjectsComponent implements OnInit {
-  builders_details: any;
+  top_projects: any;
 
   constructor(private shared : SharedService) { }
 
@@ -18,24 +18,44 @@ export class DashboardProjectsComponent implements OnInit {
   }
   
   getTopProject(){
-    let body_builders_details = { i_builder_id: '510673', Token: 'MH3NPYK34J0KHDI' };
-    console.log(body_builders_details)
+    let body_top_project = { i_builder_id: '510673', Token: 'MH3NPYK34J0KHDI' };
+   
     setTimeout(() => {
-      (<any>this.shared.client).View_bldr_top_proj(body_builders_details).subscribe(
+      (<any>this.shared.client).View_bldr_top_proj(body_top_project).subscribe(
 
         (res: ISoapMethodResponse) => {
           console.log('method response', res);
           let xmlResponse = res.xml;
           let result = res.result.View_bldr_top_projResult;
-          console.log(result);
 
           var result_json = JSON.parse(result)
           console.log("object", result_json)
 
 
-          this.builders_details = result_json.Table;
+          this.top_projects = result_json.Table;
 
-         
+          for (let index = 0; index < this.top_projects.length; index++) {
+            const element = this.top_projects[index];
+            let lead_percent = (element.CONV_LEAD / element.TOTAL_LEAD) * 100;
+            let lead_outof10 = Math.round((lead_percent / 100) * 10);
+            
+            for (let j = 1; j <= 10; j++) {
+              if(j <= lead_outof10){
+                this.top_projects[index]['u'+j] = true;
+              }else{
+                this.top_projects[index]['u'+j] = false;
+              }
+            }
+            this.top_projects[index]['disb_percent'] = (element.TOT_SANCTION / element.TOTAL_FUNDING) * 100;
+
+          }
+
+          setTimeout(() => {
+            for (let index = 0; index < this.top_projects.length; index++) {
+              $('#progress'+index).css('width',this.top_projects[index]['disb_percent']+'%');
+            }
+          }, 1);
+         console.log(this.top_projects);
         },
         err => console.log(err)
       );
