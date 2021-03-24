4fb708e289d98aa4b3e4fa4aac427beabdd8b0c7
vkpwalab <vkpwalab@gmail.com>
2021-03-24 14:57:43 +0530

    view doc

--- src/app/raise-demand/raise-demand.component.ts
+++ src/app/raise-demand/raise-demand.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { SharedService } from '../services/shared.service';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router, ActivatedRoute } from '@angular/router';
+import { toBase64String } from '@angular/compiler/src/output/source_map';
 
 @Component({
   selector: 'app-raise-demand',
@@ -56,9 +57,9 @@ export class RaiseDemandComponent implements OnInit {
                                   <soapenv:Body>
                                     <tem:get_file_list>
                                         <!--Optional:-->
-                                        <tem:i_project_id>47091</tem:i_project_id>
+                                        <tem:i_project_id>${this.project_id}</tem:i_project_id>
                                         <!--Optional:-->
-                                        <tem:i_proj_bldg_no>96354</tem:i_proj_bldg_no>
+                                        <tem:i_proj_bldg_no>${this.building_id}</tem:i_proj_bldg_no>
                                         <!--Optional:-->
                                         <tem:Token>${this.token}</tem:Token>
                                     </tem:get_file_list>
@@ -194,10 +195,10 @@ export class RaiseDemandComponent implements OnInit {
     }
   }
 
-  getFiles(sr_no,cust_name) {
+  getFiles(sr_no, cust_name) {
     let sr_no_arr = sr_no ? sr_no.split(',') : [];
     this.raise_doc = [];
-    this.cust_name_file = cust_name; 
+    this.cust_name_file = cust_name;
     sr_no_arr.forEach(element => {
       let body_get_upld_doc_name = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">
                                       <soapenv:Header/>
@@ -216,7 +217,7 @@ export class RaiseDemandComponent implements OnInit {
           let all_doc = data.Table;
           let count = 0;
           all_doc.forEach(element => {
-              this.raise_doc.push(element);
+            this.raise_doc.push(element);
           });
         }
       );
@@ -224,5 +225,57 @@ export class RaiseDemandComponent implements OnInit {
 
   }
 
+  async view_doc(doc_name, proj_id) {
+    let body_get_upld_doc_name = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:tem="http://tempuri.org/">
+                                      <soapenv:Header/>
+                                      <soapenv:Body>
+                                      <tem:View_Doc>
+                                      <tem:I_PROJECT_No>${proj_id}</tem:I_PROJECT_No>
+                                      <tem:I_DOC_CODE>${doc_name}</tem:I_DOC_CODE>
+                                      </tem:View_Doc>
+                                      </soapenv:Body>
+                                  </soapenv:Envelope>`;
+
+    let soapaction = 'http://tempuri.org/IService1/View_Doc';
+    let result_tag = 'View_DocResult';
+
+    this.shared.getDataArray(soapaction, body_get_upld_doc_name, result_tag).subscribe((res) => {
+
+
+      const mimeType = [];
+      mimeType[".xls"] = "application/vnd.ms-excel";
+      mimeType[".jpg"] = "image/jpeg";
+      mimeType[".jpeg"] = "image/jpeg";
+      mimeType[".png"] = "image/png";
+      mimeType[".gif"] = "image/svg+xml";
+      mimeType[".pdf"] = "application/pdf";
+
+      mimeType[".doc"] = "application/msword";
+      mimeType[".docx"] = "application/vnd.openxmlformats-officedocument.wordprocessingml.document";
+      mimeType[".xlsx"] = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet";
+      mimeType[".ppt"] = "application/vnd.ms-powerpoint";
+
+      console.log(mimeType);
+
+
+
+      const docArr = [];
+      for (let item of res) {
+        docArr.push(item.innerHTML)
+      }
+
+      console.log(docArr[0])
+
+
+      this.shared.downlodFileFromBase64(docArr[1], mimeType[docArr[0]], doc_name)
+
+
+
+
+
+
+    })
+
+  }
 
 }
